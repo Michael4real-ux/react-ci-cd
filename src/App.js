af5ea691c0c96231d371234fb3409891e42011fb
Michael <dammydeji.dd@gmail.com>
2023-11-08 10:48:04 +0100

    push

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to Dami
+          Edit <code>src/App.js</code> and save to Stella
         </p>
         <a
           className="App-link"
